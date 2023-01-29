07960c99ab0fd4baaaa79b309410d67347e9bdaf
WenzhuoXu <wxu2@andrew.cmu.edu>
2023-01-29 17:15:17 -0500

    Update __init__.py

--- megaflow/__init__.py
+++ megaflow/__init__.py
@@ -3,4 +3,4 @@ The MegaFLow2D dataset
 """
 __author__ = "Wenzhuo Xu, Christopher McComb, and Noelia Grande Gutierrez"
 __version__ = "0.0.1"
-__credits__ = ["Carnegie Mellon University"]
+__credits__ = "Carnegie Mellon University"
